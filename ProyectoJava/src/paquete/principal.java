dc5fe8864c304f5f1d9168d6167f8df17d649123
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-03-05 12:09:54 +0100

    commit 3

--- ProyectoJava/src/paquete/principal.java
+++ ProyectoJava/src/paquete/principal.java
@@ -3,5 +3,7 @@ package paquete;
 public class principal {
 	public static void main() {
 		System.out.println("dos mas dos son cuatro");
+		System.out.println("hola mundo");
+		System.out.println("los cubos tienen seis caras");
 	}
 }
